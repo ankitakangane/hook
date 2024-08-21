bcef4885b27063410d366d2e9b9594509637bf73
ankitakangane <kanganeankita1@gmail.com>
2024-08-21 14:01:02 +0530

    use useEffect hooks

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import React,{ useState } from 'react';
+import React,{ useState,useEffect } from 'react';
 import "./App.css";
 
 function App() {
   const [count,setCount] = useState(0);
+  useEffect(() => {
+    console.log(`Welcome To Counter App`)
+  },[]);
+  useEffect(() => {
+    console.log(`${count} is ${count % 2 == 0 ? 'Even' : 'Odd'}`)
+  },[count])
   return (
     <div className='container'>
       <h1>Use State</h1>
